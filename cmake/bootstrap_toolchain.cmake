dfbc92c1d73e095081ae9bb5aea82e01f1cf33ac
felix-pc-bar <felix.pc.barber@outlook.com>
2025-12-31 13:39:07 +0000

    switch to zip distr for mingw, cmake auto extraction

--- cmake/bootstrap_toolchain.cmake
+++ cmake/bootstrap_toolchain.cmake
@@ -1,18 +1,15 @@
-set(TOOLCHAIN_ROOT "${CMAKE_BINARY_DIR}/toolchains")
-
-if (NOT EXISTS "${TOOLCHAIN_ROOT}/mingw64/bin/g++.exe")
+if (NOT EXISTS "${CMAKE_SOURCE_DIR}/toolchains/mingw64/bin/g++.exe")
     message(STATUS "Extracting bundled MinGW toolchain...")
 
-    file(MAKE_DIRECTORY "${TOOLCHAIN_ROOT}")
+    file(MAKE_DIRECTORY "${CMAKE_SOURCE_DIR}/toolchains/mingw64")
 
     execute_process(
-        COMMAND ${CMAKE_COMMAND} -E tar x
-                "${CMAKE_SOURCE_DIR}/toolchains/mingw64.7z"
-        WORKING_DIRECTORY "${TOOLCHAIN_ROOT}"
+        COMMAND powershell -Command "Expand-Archive -Path 'mingw64.zip' -DestinationPath 'mingw64'"
+        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/toolchains"
         RESULT_VARIABLE _extract_result
     )
 
-    if (NOT _extract_result EQUAL 0)
-        message(FATAL_ERROR "Failed to extract MinGW toolchain")
-    endif()
+#    if (NOT _extract_result EQUAL 0)
+#        message(FATAL_ERROR "Failed to extract MinGW toolchain")
+#    endif()
 endif()
