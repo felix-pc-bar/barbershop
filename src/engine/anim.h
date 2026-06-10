eb4879c97885802824d123d1d9e70758359507f7
Felix Barber <felix.pc.barber@outlook.com>
2026-06-11 07:40:33 +0800

    clean up somewhat

---
+++ src/engine/anim.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <chrono>
+#include <ctime>
+
+using dtclock = std::chrono::steady_clock;
+
+class anim
+{
+	// start time
+	// end time
+	// some kind of function for the easing
+	// TODO: learn std::chrono
+};
--- src/engine/game.cpp
+++ src/engine/game.cpp
@@ -211,10 +211,10 @@ void Game::run()
 				if (event.type == SDL_MOUSEWHEEL)
 				{
 					// wheel
-					int mouseDX = static_cast<int>(dx * 1) - static_cast<int>(mousex / 1);
-					int mouseDY = static_cast<int>(dy * 1) - static_cast<int>(mousey / 1);
+					int mouseDX = dx - mousex;
+					int mouseDY = dy - mousey;
 					int deltaScale = 0;
-					if (event.wheel.y > 0)
+					if (event.wheel.y > 0 && scale < 64)
 					{
 						deltaScale = std::round(scale * 1.75 - scale);
 					}
@@ -223,24 +223,22 @@ void Game::run()
 						deltaScale = std::round(scale / 1.75 - scale);
 					}
 					float scaleMultiplicand = (static_cast<float>(scale + deltaScale) / static_cast<float>(scale)) - 1.0f;
-					if (scaleMultiplicand != 0)
-					{
-						std::cout << scaleMultiplicand << std::endl;
-					}
 					scale += deltaScale;
 					dx += mouseDX * scaleMultiplicand;
 					dy += mouseDY * scaleMultiplicand;
 				}
 
 				// Key hold events here
-
 				// if (gk[SDL_SCANCODE_LEFT]) { dx -= 20 * dtFac; }
-				// if (gk[SDL_SCANCODE_RIGHT]) { dx += 20 * dtFac; }
-				// if (gk[SDL_SCANCODE_DOWN]) { dy -= 20 * dtFac; }
-				// if (gk[SDL_SCANCODE_UP]) { dy += 20 * dtFac; }
 
 				if (event.type == SDL_KEYDOWN && event.key.repeat == 0)
 				{
+					if (event.key.keysym.sym == SDLK_HOME)
+					{
+						dx = globScreenwidth / 2;
+						dy = globScreenheight / 2;
+						scale = 1;
+					}
 					if (event.key.keysym.sym == SDLK_s)
 					{
 						const char* fp = tinyfd_saveFileDialog(
